fe9a8db8b3354fdb86ea623c227de61b0fea1262
LiuPineapple <1308153527@qq.com>
2019-11-18 08:58:29 +0800

    add 011

---
+++ 剑指Offer/C++_Practice/011-旋转数组的最小数字/011-旋转数组的最小数字/Answer_repeat.cpp
@@ -0,0 +1,103 @@
+//二分查找适用于排序数组，时间复杂度为O(log(n))
+//改了while的条件，运行结果没有区别
+#include<exception>
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+//顺序查找
+int MinInorder(int * number, int index1, int index2)
+{
+	int length = index2 - index1 + 1;
+	int result = number[0];
+	for (int i = 1; i <= length; i++)
+	{
+		if (result < number[i])
+			result = number[i];
+	}
+	return result;
+}
+
+//二分查找
+int Min(int * number, int length)
+{
+	if (number == nullptr || length <= 0)
+		throw new exception("Invalide Input");
+	int index1 = 0;
+	int index2 = length - 1;
+	int indexM = index1;
+	while (index2>index1)
+	{
+		if (index2 - index1 == 1)
+		{
+			indexM = index2;
+			break;
+		}
+		indexM = (index1 + index2) / 2;
+		if (number[index2] == number[index1] && number[index1] == number[indexM])
+			return MinInorder(number, index1, index2);
+		if (number[indexM] >= number[index1])
+			index1 = indexM;
+		if (number[indexM] <= number[index2])
+			index2 = indexM;
+	}
+	return number[indexM];
+}
+
+	// ====================测试代码====================
+	void Test(int* numbers, int length, int expected)
+	{
+		int result = 0;
+		try
+		{
+			result = Min(numbers, length);
+
+			for (int i = 0; i < length; ++i)
+				printf("%d ", numbers[i]);
+
+			if (result == expected)
+				printf("\tpassed\n");
+			else
+				printf("\tfailed\n");
+		}
+		catch (...)
+		{
+			if (numbers == nullptr)
+				printf("Test passed.\n");
+			else
+				printf("Test failed.\n");
+		}
+	}
+
+	int main(int argc, char* argv[])
+	{
+		// 典型输入，单调升序的数组的一个旋转
+		int array1[] = { 3, 4, 5, 1, 2 };
+		Test(array1, sizeof(array1) / sizeof(int), 1);
+
+		// 有重复数字，并且重复的数字刚好的最小的数字
+		int array2[] = { 3, 4, 5, 1, 1, 2 };
+		Test(array2, sizeof(array2) / sizeof(int), 1);
+
+		// 有重复数字，但重复的数字不是第一个数字和最后一个数字
+		int array3[] = { 3, 4, 5, 1, 2, 2 };
+		Test(array3, sizeof(array3) / sizeof(int), 1);
+
+		// 有重复的数字，并且重复的数字刚好是第一个数字和最后一个数字
+		int array4[] = { 1, 0, 1, 1, 1 };
+		Test(array4, sizeof(array4) / sizeof(int), 0);
+
+		// 单调升序数组，旋转0个元素，也就是单调升序数组本身
+		int array5[] = { 1, 2, 3, 4, 5 };
+		Test(array5, sizeof(array5) / sizeof(int), 1);
+
+		// 数组中只有一个数字
+		int array6[] = { 2 };
+		Test(array6, sizeof(array6) / sizeof(int), 2);
+
+		// 输入nullptr
+		Test(nullptr, 0, 0);
+		system("pause");
+		return 0;
+	}
+
